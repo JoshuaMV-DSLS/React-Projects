a456af4d7b77faf61b8dddc582c336fa26886cac
DSLS <josh_9925@hotmail.com>
2026-05-09 22:24:20 -0600

    jsconfig.json modified

--- GameProject/src/App.jsx
+++ GameProject/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
-import './App.css'
+import Timer from './components/timer'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>Welcome to My Game</h1>
+    <>
+      <h1>Welcome to My Game</h1>
+      <Timer />
+      <button onClick={() => setCount(count + 1)}>Count: {count}</button>
+    </>
   )
 } 
   
