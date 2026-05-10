d5afd849706b2a8d6fc2324b185f36927aee1abb
DSLS <josh_9925@hotmail.com>
2026-05-09 23:23:08 -0600

    gameproject modified:   GameProject/src/App.jsx

--- GameProject/src/App.jsx
+++ GameProject/src/App.jsx
@@ -1,15 +1,28 @@
-import { useState } from 'react'
-import Timer from './components/timer'
+import { useState,useEffect } from 'react'
 
 
 function App() {
   const [count, setCount] = useState(0)
+  const [time, setTime] = useState(0)
+
+  useEffect(() => {
+
+     if (count<1) {
+      setCount(0)
+    }
+        const interval = setInterval(() => {
+            setTime(prevTime => prevTime + 1)
+        }, 120)
+
+        return () => clearInterval(interval)
+    }, [])
+
 
   return (
     <>
       <h1>Welcome to My Game</h1>
-      <Timer />
-      <button onClick={() => setCount(count + 1)}>Count: {count}</button>
+        <h2>Time: {time}</h2>
+      <button onClick={() => setCount(count + 1)}>Click Me: {count}</button>
     </>
   )
 } 
